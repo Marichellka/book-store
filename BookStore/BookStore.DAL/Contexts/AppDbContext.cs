2bb19017a766d84a46128ba8d3e80d1106bd672d
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-10-26 23:33:35 +0300

    add db context

---
+++ BookStore/BookStore.DAL/Contexts/AppDbContext.cs
@@ -0,0 +1,98 @@
+﻿using BookStore.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DAL.Contexts;
+
+public class AppDbContext: DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Publisher> Publishers { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<BookCategory> BookCategories { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+    { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Book>()
+            .HasOne(b => b.Author)
+            .WithMany(a => a.Books)
+            .HasForeignKey(b => b.AuthorId)
+            .IsRequired();
+
+        builder.Entity<Book>()
+            .HasOne(b => b.Publisher)
+            .WithMany(p => p.Books)
+            .HasForeignKey(b => b.PublisherId)
+            .IsRequired();
+
+        builder.Entity<BookCategory>()
+            .HasOne<Book>(bc => bc.Book)
+            .WithMany(b => b.Categories)
+            .HasForeignKey(bc => bc.BookId)
+            .IsRequired();
+
+        builder.Entity<BookCategory>()
+            .HasOne<Category>(bc => bc.Category)
+            .WithMany(c => c.BookCategories)
+            .HasForeignKey(bc => bc.CategoryId)
+            .IsRequired();
+
+        builder.Entity<Review>()
+            .HasOne(r => r.Book)
+            .WithMany(b => b.Reviews)
+            .HasForeignKey(r => r.BookId)
+            .IsRequired();
+
+        builder.Entity<Review>()
+            .HasOne(r => r.User)
+            .WithMany(u => u.Reviews)
+            .HasForeignKey(r => r.UserId)
+            .IsRequired();
+
+        builder.Entity<Order>()
+            .HasOne(o => o.User)
+            .WithMany(u => u.Orders)
+            .HasForeignKey(o => o.UserId)
+            .IsRequired();
+
+        builder.Entity<OrderItem>()
+            .HasOne(oi => oi.Order)
+            .WithMany(o => o.OrderItems)
+            .HasForeignKey(oi => oi.OrderId)
+            .IsRequired();
+
+        builder.Entity<OrderItem>()
+            .HasOne(oi => oi.Book)
+            .WithMany(o => o.OrderItems)
+            .HasForeignKey(oi => oi.BookId)
+            .IsRequired();
+
+        builder.Entity<User>()
+            .HasOne(u => u.Cart)
+            .WithOne(c => c.User)
+            .HasForeignKey<Cart>(c => c.Id)
+            .IsRequired();
+
+        builder.Entity<CartItem>()
+            .HasOne(ci => ci.Book)
+            .WithMany(b => b.CartItems)
+            .HasForeignKey(ci => ci.BookId)
+            .IsRequired();
+        
+        builder.Entity<CartItem>()
+            .HasOne(ci => ci.Cart)
+            .WithMany(c => c.CartItems)
+            .HasForeignKey(ci => ci.Cart)
+            .IsRequired();
+    }
+
+}

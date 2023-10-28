fecbbc22d35569a545a7b8d48350bde567993d8f
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-10-28 18:37:30 +0300

    add repositories

---
+++ BookStore/BookStore.DAL/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.DAL.Models;
+
+namespace BookStore.DAL.Repositories;
+
+public interface  IRepository<TModel> where TModel : BaseModel
+{
+    Task<List<TModel>> GetAll();
+
+    Task<TModel?> GetById(int id);
+
+    Task Add(TModel entity);
+
+    Task Delete(TModel entity);
+
+    Task Update(TModel entity);
+}
---
+++ BookStore/BookStore.DAL/Repositories/Repository.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.DAL.Contexts;
+using BookStore.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DAL.Repositories;
+
+public class Repository<TModel>: IRepository<TModel> where TModel : BaseModel
+{
+    private readonly AppDbContext _context;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<TModel>> GetAll()
+    {
+        return _context.Set<TModel>().ToListAsync();
+    }
+
+    public Task<TModel?> GetById(int id)
+    {
+        return _context.Set<TModel>().FindAsync(id).AsTask();
+    }
+
+    public async Task Add(TModel entity)
+    {
+        await _context.Set<TModel>().AddAsync(entity);
+    }
+
+    public async Task Delete(TModel entity)
+    {
+        _context.Set<TModel>().Remove(entity);
+    }
+
+    public async Task Update(TModel entity)
+    {
+        _context.Set<TModel>().Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+}

c8b731832fb6230e15698ae1c25045238a28a7dd
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-11-01 14:59:26 +0200

    add WebApp configuration

---
+++ BookStore/BookStore.WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using BookStore.BLL.MappingProfiles;
+using BookStore.BLL.Services;
+using BookStore.DAL.Contexts;
+using BookStore.DAL.UnitOfWork;
+
+namespace BookStore.WebApi.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<UserService>();
+        services.AddScoped<AuthorService>();
+        services.AddScoped<PublisherService>();
+        services.AddScoped<BookService>();
+        services.AddScoped<CartService>();
+        services.AddScoped<OrderService>();
+        services.AddScoped<ReviewService>();
+        services.AddScoped<CategoryService>();
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<AppDbContext>();
+    }
+    
+    public static  void ConfigureServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(cfg => {
+                cfg.AddProfile<UserProfile>(); 
+                cfg.AddProfile<AuthorProfile>(); 
+                cfg.AddProfile<PublisherProfile>(); 
+                cfg.AddProfile<BookProfile>(); 
+                cfg.AddProfile<CartProfile>(); 
+                cfg.AddProfile<OrderProfile>(); 
+                cfg.AddProfile<ReviewProfile>(); 
+                cfg.AddProfile<CategoryProfile>();
+            },
+            Assembly.GetExecutingAssembly());
+    }
+
+}
--- BookStore/BookStore.WebApi/Program.cs
+++ BookStore/BookStore.WebApi/Program.cs
@@ -1,6 +1,46 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using BookStore.DAL.Contexts;
+using BookStore.WebApi.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
-app.MapGet("/", () => "Hello World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")!));
+        builder.Services.AddControllers();
+        
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.CustomSchemaIds(type => type.ToString());
+            c.SwaggerDoc("v1", new OpenApiInfo{ Title = "BookStore.WebAPI", Version = "v1" });
+        });
+
+        
+        builder.Services.RegisterServices();
+        builder.Services.ConfigureServices(); ;
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+        
+        app.MapControllers();
+
+        app.Run();
+    }
+};

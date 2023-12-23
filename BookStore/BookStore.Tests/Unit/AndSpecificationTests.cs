f55cbe17b553e08f49c7a75b8821aa95e42c125f
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-12-23 20:47:02 +0200

    add basic specification tests

---
+++ BookStore/BookStore.Tests/Unit/AndSpecificationTests.cs
@@ -0,0 +1,40 @@
+﻿using BookStore.DAL.Models;
+using BookStore.DAL.Specifications;
+
+namespace BookStore.Tests.Unit;
+
+public class AndSpecificationTests: SpecificationTestBase
+{
+    private ISpecification<BaseModel> _trueSpecification = new TrueSpecification<BaseModel>();
+    private ISpecification<BaseModel> _falseSpecification = new FalseSpecification<BaseModel>();
+
+    [Test]
+    public void TrueAndTrue_Satisfies()
+    {
+        AndSpecification<BaseModel> specification = new(_trueSpecification, _trueSpecification);
+
+        bool result = IsSatisfiedBy(specification, new BaseModel());
+        
+        Assert.True(result);
+    }
+
+    [Test]
+    public void TrueAndFalse_DoesNotSatisfy()
+    {
+        AndSpecification<BaseModel> specification = new(_trueSpecification, _falseSpecification);
+
+        bool result = IsSatisfiedBy(specification, new BaseModel());
+        
+        Assert.False(result);
+    }
+
+    [Test]
+    public void FalseAndFalse_DoesNotSatisfy()
+    {
+        AndSpecification<BaseModel> specification = new(_falseSpecification, _falseSpecification);
+
+        bool result = IsSatisfiedBy(specification, new BaseModel());
+        
+        Assert.False(result);
+    }
+}

5dfc909fd4c1f98052b1dd30a3769ee1e7a516ae
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-12-23 21:25:40 +0200

    add cart specification tests

---
+++ BookStore/BookStore.Tests/Unit/CartSpecificationTests.cs
@@ -0,0 +1,47 @@
+﻿using BookStore.DAL.Models;
+using BookStore.DAL.Specifications.Cart;
+
+namespace BookStore.Tests.Unit;
+
+public class CartSpecificationTests : SpecificationTestBase
+{
+    [Test]
+    public void CartItemsSpecification_Satisfies()
+    {
+        CartItemsSpecification specification = new(cartId: 1);
+
+        bool result = IsSatisfiedBy(specification, new CartItem { CartId = 1 });
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void CartItemsSpecification_DoesNotSatisfy()
+    {
+        CartItemsSpecification specification = new(cartId: 1);
+
+        bool result = IsSatisfiedBy(specification, new CartItem { CartId = 2 });
+
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void UserCartSpecification_Satisfies()
+    {
+        UserCartSpecification specification = new(userId: 1);
+
+        bool result = IsSatisfiedBy(specification, new Cart { UserId = 1 });
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void UserCartSpecification_DoesNotSatisfy()
+    {
+        UserCartSpecification specification = new(userId: 1);
+
+        bool result = IsSatisfiedBy(specification, new Cart { UserId = 2 });
+
+        Assert.That(result, Is.False);
+    }
+}

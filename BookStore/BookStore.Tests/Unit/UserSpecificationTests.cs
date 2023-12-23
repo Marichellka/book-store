51cbc1b6ebe7fd1b8518dc55a579567d03751620
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-12-23 21:25:00 +0200

    add user specification tests

---
+++ BookStore/BookStore.Tests/Unit/UserSpecificationTests.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.DAL.Models;
+using BookStore.DAL.Specifications.Users;
+
+namespace BookStore.Tests.Unit;
+
+public class UserSpecificationTests : SpecificationTestBase
+{
+    [Test]
+    public void UserNameSpecification_Satisfies()
+    {
+        UserNameSpecification specification = new(name: "User");
+
+        bool result = IsSatisfiedBy(specification, new User { Name = "User" });
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void UserNameSpecification_DoesNotSatisfy()
+    {
+        UserNameSpecification specification = new(name: "User1");
+
+        bool result = IsSatisfiedBy(specification, new User { Name = "User2" });
+
+        Assert.That(result, Is.False);
+    }
+}
